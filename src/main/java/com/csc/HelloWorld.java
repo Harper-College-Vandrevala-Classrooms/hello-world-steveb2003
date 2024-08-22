51b73757aa1a1a0b6c8f1d99ab5e71f3bb07c379
steveb2003 <nn80299@mail.harpercollege.edu>
2024-08-21 21:31:31 -0500

    Hello World Commit
    Hello World Commit Stephen Barretto

--- src/main/java/com/csc/HelloWorld.java
+++ src/main/java/com/csc/HelloWorld.java
@@ -1,3 +1,8 @@
 package com.csc;
 
 // Your code goes here!
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
